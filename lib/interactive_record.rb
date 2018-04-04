985fc3aab15e7316599e4ed96e2643457e2c26c4
Joma Ejercito <jomaejercito@gmail.com>
2018-04-04 02:25:02 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -23,7 +23,7 @@ class InteractiveRecord
 
   def initialize(options={})
     options.each do|property, value|
-      self.send("#{porperty}=", value)
+      self.send("#{property}=", value)
     end
   end
 
